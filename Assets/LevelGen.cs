05de48238cf09e625a134f45e2d1ae233890fba8
ArTeGaS-2 <probablytorvik@gmail.com>
2025-10-18 19:54:45 +0300

    Теги, генератор, перешкоди

--- Assets/LevelGen.cs
+++ Assets/LevelGen.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class LevelGen : MonoBehaviour
 {
-    public GameObject scenePartPrefab; // Сегмент сцени
+    [Header("Об'єкти")]
+    public GameObject basePartPrefab; // Базовий сегмент сцени
+    public List<GameObject> anotherParts =
+        new List<GameObject>(); // Список інакших сегментів
+
+    [Header("Налаштування")]
     public float scenePartStep; // Крок від сегменту до іншого
     public float basePartPos; // Позиція базового фрагменту
     public int numOfParts; // Кількість фрагментів
@@ -12,11 +17,20 @@ public class LevelGen : MonoBehaviour
     {
         for (int i = 1; i <= numOfParts; i++)
         {
-            Instantiate(scenePartPrefab,
-                new Vector3(0, 0,
+            if (i == 3 || i == 4 || i == 5)
+            {
+                Instantiate(anotherParts[0],
+                    new Vector3(0, 0,
                     basePartPos + i * scenePartStep),
-                Quaternion.identity);
+                    Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(basePartPrefab,
+                    new Vector3(0, 0,
+                    basePartPos + i * scenePartStep),
+                    Quaternion.identity);
+            }
         }
     }
-
 }
