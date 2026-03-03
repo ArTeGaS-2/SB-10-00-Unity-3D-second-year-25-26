37e88ec1ca7ce8312fe22bab9144a9bc8eaa40c6
ArTeGaS-2 <probablytorvik@gmail.com>
2026-03-03 13:00:36 +0200

    Самостійна робота: реалізували рух по траєкторії вісьмірки та літери S.

---
+++ Assets/Scenes/Endless.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endless : MonoBehaviour
+{
+    // Умови if та else if
+    // transform.Rotate
+    // transform.Translate або rb.velocity
+
+    // Складність 1 - Змусьте об'єкт рухатись вісьміркою
+    // Складність 2 - Змусьте об'єкт рухатимь як літера "S" і назад
+}
