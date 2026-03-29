95e4534d7a2fc865e0970807ee500d5a664fd545
ArTeGaS-2 <probablytorvik@gmail.com>
2026-03-29 10:33:47 +0300

    Налаштували політ проджектайла до цілі

--- Assets/TowerDF/Tower/Projectile.cs
+++ Assets/TowerDF/Tower/Projectile.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [HideInInspector] public GameObject targetObj; // До кого летить куля
+    [SerializeField] private float projectileSpeed = 10.0f; // Швидкість кулі
 
+    public void SetTargetObject(GameObject target) { targetObj = target; }
+
+    private void FixedUpdate()
+    {
+        // рух до цілі
+        transform.position = Vector3.MoveTowards(
+            transform.position, // звідки
+            targetObj.transform.position, // куди
+            projectileSpeed * Time.fixedDeltaTime); // з якою швидкістю
+
+        transform.LookAt(targetObj.transform.position); // дивимось на ціль
+    }
 }
